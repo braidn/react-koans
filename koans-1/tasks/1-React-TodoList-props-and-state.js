7da676e536f4fd0a8761d7fd13d99855e409711e
Braden <braden.douglass@gmail.com>
2017-03-31 00:52:01 -0400

    Green Up 1st Koan.
    Plus a little bit of refactoring in the second part. Text based props
    are no longer a good thing in the React ecosystem and will soon be
    deprecated.

--- koans-1/tasks/1-React-TodoList-props-and-state.js
+++ koans-1/tasks/1-React-TodoList-props-and-state.js
@@ -1,42 +1,45 @@
-import React from "react";
+import React from 'react'
 
-// Task: Fill the `return` of `TodoList` class. 
+// Task: Fill the `return` of `TodoList` class.
 // It should return a list of `ListItem`. You need to display the task name.
 
 // Minimize the state in your components and get data using props from parent components whenever possible.
 // In the example ListItems shouldn't have state
 class TodoList extends React.Component {
-  constructor() {
-    super();
+  constructor () {
+    super()
     this.state = {
-      tasks: [ { name: "Learn more JavaScript" },
-               { name: "Learn more testing too!" }
-             ]
-    };
+      tasks: [ { name: 'Learn more JavaScript' },
+               { name: 'Learn more testing too!' }
+      ]
+    }
   }
 
-  render() {
+  render () {
     // Properties can pass data to React components. The sintax is similar to HTML elements' attributs.
     // Below you can see how to pass data to a child component using a 'task' property
-    let tasksComponents = this.state.tasks.map( task => (
-        <ListItem />)
+    let key = 0
+    const tasksComponents = this.state.tasks.map(task => (
+      <ListItem task={task} key={key++} />)
     )
 
     // Hint: Don't forget to wrap it with an `ul` element
     return (
       <div>
-          // Write some code here
+        <ul>
+          {tasksComponents}
+        </ul>
       </div>
-    );
+    )
   }
 }
 
 // Render task name from the component's properties.
 // Don't you know how?  Some key words: interpolate react props
-export const ListItem = props =>(
-    <li>
-          // Write some code here
-    </li>
-);
+export const ListItem = props => (
+  <li>
+    {props.task.name}
+  </li>
+)
 
-export default TodoList;
+export default TodoList
--- koans-1/tasks/2-React-TodoList-one-way-data-binding.js
+++ koans-1/tasks/2-React-TodoList-one-way-data-binding.js
@@ -1,58 +1,53 @@
-import React from "react";
+import React from 'react'
 
 // Users should be able to add tasks to the list.
 // Task 1: Add the newTaskAddButton to the render and bind it to a the component method addTask
 // Task 2: Implement the `addTask` method. This method should modify the `ToDoList`
-// component's state. Changing the state will render this component and its child components. 
+// component's state. Changing the state will render this component and its child components.
 // Changes in the state must be explicit. That's called one-way data-binding
 
 class TodoList extends React.Component {
-  constructor() {
-    super();
+  constructor () {
+    super()
     this.state = {
+      newTask: '',
       tasks: [
         { name: "Learn more advance React, it's awesome!" }
       ]
-    };
-
-    this.inputChanged = this.inputChanged.bind(this);
-  }
-
-  inputChanged(event) {
-    //this method is just a hint of how you can implement the addTask function
-    console.log(this.refs.txt.value);
+    }
+    this.addTask = this.addTask.bind(this)
   }
 
   // Task 2. Implement this method to add new tasks to the list
-  addTask() {
-    // You need to change this.state using this.setState({your new state}) otherwhise React doesn´t track your changes, 
-    // in other words it doesn't re-render the task list
-    let tasks = { tasks: [...this.state.tasks] };
-    this.setState(tasks);
+  addTask () {
+    let tasks = { tasks: [...this.state.tasks, {name: this.textInput.value}] }
+    this.setState(tasks)
   }
 
-  render() {
+  render () {
     let tasksComponents = this.state.tasks.map((task, index) => (
-        <ListItem />)
+      <ListItem key={index} task={task} />)
     )
-    let newTaskInput = <input ref="txt" type="text" onChange={this.inputChanged}/>;
-    let newTaskAddButton = <button>Add new Product</button>;
-  
+    let newTaskInput = <input ref={(input) => { this.textInput = input }}
+      type='text' />
+    let newTaskAddButton = <button onClick={this.addTask}>Add new Product</button>
+
     return (
       <div>
         <ul>
-          // Write some code here
+          {tasksComponents}
         </ul>
-        // Write some code here
+        {newTaskInput}
+        {newTaskAddButton}
       </div>
-    );
+    )
   }
 }
 
 export const ListItem = props => (
-    <li>
-        // Write some code here
-    </li>
+  <li>
+    {props.task.name}
+  </li>
 )
 
-export default TodoList;
+export default TodoList
