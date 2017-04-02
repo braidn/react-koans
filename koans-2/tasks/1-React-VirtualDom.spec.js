756f99092ab38c99665d12067fa17878ecfa5f2a
Braden <braden.douglass@gmail.com>
2017-04-02 00:09:13 -0400

    Green Up Koan 2

--- koans-2/tasks/1-React-VirtualDom.spec.js
+++ koans-2/tasks/1-React-VirtualDom.spec.js
@@ -1,71 +1,54 @@
-import TodoList,{ListItem} from '../../koans-1/tasks/2-React-TodoList-one-way-data-binding.js';
-import React from "react";
-import ReactDOM from "react-dom";
-import TestUtils from "react-addons-test-utils";
-import { expect } from "chai";
+/* global describe it */
+import TodoList, {ListItem} from '../../koans-1/tasks/2-React-TodoList-one-way-data-binding.js'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import TestUtils from 'react-addons-test-utils'
+import { shallow } from 'enzyme'
+import { expect } from 'chai'
 
-describe("Virtual DOM test. React Compoment, React Instance and React Element:", () => {
-  
-  it("JsX creates React Elements.", () => {
-    let am_I_a_component = <TodoList/>;
-    
-    // change this var
-    let change_me = '????';
+describe('Virtual DOM test. React Compoment, React Instance and React Element:', () => {
+  it('JsX creates React Elements.', () => {
+    const amIAComponent = <TodoList />
+    const elementType = 'react.element'
 
-    // hint, uncomment next line to see the object in the console:
-    // console.log(am_I_a_component);
+    expect(`Symbol(${elementType})`).to.be.equal(amIAComponent['$$typeof'].toString())
+  })
 
-    expect(`Symbol(${change_me})`).to.be.equal(am_I_a_component['$$typeof'].toString());
-  });
+  it('JsX creates new objects every time.', () => {
+    const component1 = <TodoList />
+    const component2 = <TodoList />
 
-  it("JsX creates new objects every time.", () => {
-    let component_1 = <TodoList/>;
-    let component_2 = <TodoList/>;
-    
     // change this assertion, are the two React Elements deeply equal?
-    expect(component_1).not.to.be.deep.equal(component_2);
-  });
+    // the objects are deeply equal but they are different instances so a simple equal works
+    expect(component1).not.to.be.equal(component2)
+  })
 
   it("React Elements do not implement component's functionality.", () => {
-    let reactElement = <TodoList/>;
+    const reactElement = <TodoList />
 
-    // hint, uncomment next line to see the object in the console:
-    // console.log('--------->',reactElement);
+    // An assertion library would give you many clues and it would be too easy, that's why we are throwing an Error with a custome message instead
+    expect(reactElement.addTask).to.be.undefined
+  })
 
-    //An assertion library would give you many clues and it would be too easy, that's why we are throwing an Error with a custome message instead
-    if ('CHANGE THIS' !== typeof reactElement.addTask) {
-      throw new Error("The typeof(reactElement.addTask) is not correct");
-    }
-  });
-  
-  it("Rendering into the document should return a React Instance.", () => {
-    let component = TestUtils.renderIntoDocument(<TodoList/>);
-
-    // hint, uncomment next line to see the object in the console:
-    // console.log('--------->',component);
+  it('Rendering into the document should return a React Instance.', () => {
+    const component = TestUtils.renderIntoDocument(<TodoList />)
 
     // An assertion library would give you many clues and it would be too easy, that's why we are throwing an Error with a custome message instead
     // Change the following condition
-    if ('CHANGE THIS' !== typeof component.addTask) {
-      throw new Error("The typeof(reactElement.addTask) is not correct");
-    }
-  });
+    expect(component.addTask).to.be.a('function')
+  })
 
-  it("TestUtils.renderIntoDocument should return a detached React Instance.", () => {
-    let detachedComp_1 = TestUtils.renderIntoDocument(<TodoList/>);
-    let detachedComp_2 = TestUtils.renderIntoDocument(<TodoList/>);
+  it('TestUtils.renderIntoDocument should return a detached React Instance.', () => {
+    const detachedComp1 = TestUtils.renderIntoDocument(<TodoList />)
+    const detachedComp2 = TestUtils.renderIntoDocument(<TodoList />)
 
-    if (detachedComp_1 === detachedComp_1) {
-      throw new Error("Are you sure detachedComp_1 and detachedComp_2 are the same instance?");
-    }
-  });
-  
-  it("ReactDOM.render should return the same React Instance for the same component (they are not detached).", () => {
-    let component_1 = ReactDOM.render(<TodoList/>, document.getElementById('app'));
-    let component_2 = ReactDOM.render(<TodoList/>, document.getElementById('app'));
-    
-    if (component_1 === component_2) {
-      throw new Error("Are you sure component_1 and component_2 are the same instance?");
-    }
-  });
-});
+    expect(detachedComp1).to.not.be.eq(detachedComp2)
+  })
+
+  it('Shallow render should return the same React type for the same component (they are not detached).', () => {
+    const component1 = shallow(<TodoList />)
+    const component2 = shallow(<TodoList />)
+
+    expect(component1.type()).to.eq(component2.type())
+  })
+})
